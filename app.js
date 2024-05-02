8452be9315f1a948da46310d96a44a1c36923069
CodeByBlazej <codebyblazej@gmail.com>
2024-05-02 11:16:35 +0100

    add operate function that calls other functions

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let numOne = 1;
-let numTwo = 2;
+let numOne = 2;
+let numTwo = 4;
 let operator = {
   add: '+',
   subtract: '-',
   multiply: '*',
   divide: '/'
 }
-
 
 
 
@@ -26,10 +25,23 @@ const divide = function(a, b){
   return a / b;
 }
 
-console.log(add(1, 2));
+console.log(add(numOne, numTwo));
 console.log(subtract(2, 1));
 console.log(multiply(2, 2));
 console.log(divide(12, 2));
 
 
+const operate = function(num1, op, num2){
+  if(op === operator.add){
+    return add(num1, num2);
+  } else if(op === operator.subtract){
+    return subtract(num1, num2);
+  } else if(op === operator.multiply){
+    return multiply(num1, num2);
+  } else if(op === operator.divide){
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate(numOne, operator.add, numTwo));
 
