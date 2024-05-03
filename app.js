68f5a7a2a7164aecc3078f2dde5f5d2287319df7
CodeByBlazej <codebyblazej@gmail.com>
2024-05-03 12:03:33 +0100

    add functions to hold both values after adding

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ const display = document.querySelector(".display")
 
 
 
-
-// let displayValue = display.textContent;
-
-
-
 // let numOne = currentValue;
 let numTwo = 4;
 let operator = {
@@ -73,36 +68,53 @@ const operate = function(num1, op, num2){
 // console.log(operate(numOne, operator.add, numTwo));
 
 let currentValue = [];
+let displayedNumberMemoryOne = 0;
+let displayedNumberMemoryTwo = 0;
 
-const displayValue = function(){
+
+const displayValueOne = function(){
   let number = currentValue.toString().split(',').join('');
+  if (currentValue.length !== 0 && displayedNumberMemoryOne >= 0) {
+    displayedNumberMemoryOne = Number(number);
+  } else {
+    displayedNumberMemoryTwo = Number(number);
+  }
   return display.textContent = number;
 }
+
+// const displayValueTwo = function(){
+//   let number = currentValue.toString().split(',').join('');
+//   displayedNumberMemoryTwo = Number(number);
+//   return display.textContent = number;
+// }
+
+
 
 
 btn_0.addEventListener('click', () => {
   currentValue.push(0);
-  displayValue();
+  displayValueOne();
+  
 })
 
 btn_1.addEventListener('click', () => {
   currentValue.push(1);
-  displayValue();
+  displayValueOne();
 })
 
 btn_2.addEventListener('click', () => {
   currentValue.push(2);
-  displayValue();
+  displayValueOne();
 })
 
 btn_3.addEventListener('click', () => {
   currentValue.push(3);
-  displayValue();
+  displayValueOne();
 })
 
 btn_4.addEventListener('click', () => {
   currentValue.push(4);
-  displayValue();
+  displayValueOne();
   // display.textContent = 4;
   // if(currentValue >= 0) {
   //   currentValue = 4;
@@ -111,7 +123,7 @@ btn_4.addEventListener('click', () => {
 
 btn_5.addEventListener('click', () => {
   currentValue.push(5);
-  displayValue();
+  displayValueOne();
   // display.textContent = 5;
   // if(currentValue >= 0) {
   //   currentValue = 5;
@@ -120,7 +132,7 @@ btn_5.addEventListener('click', () => {
 
 btn_6.addEventListener('click', () => {
   currentValue.push(6);
-  displayValue();
+  displayValueOne();
   // display.textContent = 6;
   // if(currentValue >= 0) {
   //   currentValue = 6;
@@ -129,7 +141,7 @@ btn_6.addEventListener('click', () => {
 
 btn_7.addEventListener('click', () => {
   currentValue.push(7);
-  displayValue();
+  displayValueOne();
   // display.textContent = 7;
   // if(currentValue >= 0) {
   //   currentValue = 7;
@@ -138,7 +150,7 @@ btn_7.addEventListener('click', () => {
 
 btn_8.addEventListener('click', () => {
   currentValue.push(8);
-  displayValue();
+  displayValueOne();
   // display.textContent = 8;
   // if(currentValue >= 0) {
   //   currentValue = 8;
@@ -148,12 +160,21 @@ btn_8.addEventListener('click', () => {
 
 btn_9.addEventListener('click', () => {
   currentValue.push(9);
-  displayValue();
+  displayValueOne();
   // display.textContent = 9;
   // if(currentValue >= 0) {
   //   currentValue = 9;
   // }
   // console.log(currentValue);
+})
+
+btnAdd.addEventListener('click', () => {
+  currentValue = [];
+  displayedNumberMemoryTwo = displayedNumberMemoryOne;
+
+  // let a = displayedNumberMemory;
+  // let number = numOne.toString().split(',').join('');
+
 })
 
 btnClear.addEventListener('click', () => {
