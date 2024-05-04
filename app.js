e65617e0c189e5813a0d7b010e37fd0919fdd9ee
CodeByBlazej <codebyblazej@gmail.com>
2024-05-04 08:47:30 +0100

    make calculator work

--- app.js
+++ app.js
@@ -20,73 +20,62 @@ const display = document.querySelector(".display")
 
 
 
-// let numOne = currentValue;
-let numTwo = 4;
+
 let operator = {
   add: '+',
   subtract: '-',
   multiply: '*',
   divide: '/'
-}
+};
 
 
 
 const add = function(a, b){
-  return a + b;
-}
+  let result = a + b;
+  return display.textContent = result;
+};
 
 const subtract = function(a, b){
-  return a - b;
-}
+  let result = a - b;
+  return display.textContent = result;
+};
 
 const multiply = function(a, b){
-  return a * b;
-}
+  let result = a * b;
+  return display.textContent = result;
+};
 
 const divide = function(a, b){
-  return a / b;
-}
+  let result = a / b;
+  return display.textContent = result;
+};
 
-// console.log(add(numOne, numTwo));
-// console.log(subtract(2, 1));
-// console.log(multiply(2, 2));
-// console.log(divide(12, 2));
 
 
 const operate = function(num1, op, num2){
-  if(op === operator.add){
+  if(op === '+'){
     return add(num1, num2);
-  } else if(op === operator.subtract){
+  } else if(op === '-'){
     return subtract(num1, num2);
-  } else if(op === operator.multiply){
+  } else if(op === '*'){
     return multiply(num1, num2);
-  } else if(op === operator.divide){
+  } else if(op === '/'){
     return divide(num1, num2);
   }
-}
+};
 
-// console.log(operate(numOne, operator.add, numTwo));
 
 let currentValue = [];
 let displayedNumberMemoryOne = 0;
 let displayedNumberMemoryTwo = 0;
+let selectedOperator = '';
 
 
 const displayValueOne = function(){
   let number = currentValue.toString().split(',').join('');
-  if (currentValue.length !== 0 && displayedNumberMemoryOne >= 0) {
-    displayedNumberMemoryOne = Number(number);
-  } else {
-    displayedNumberMemoryTwo = Number(number);
-  }
+  displayedNumberMemoryOne = Number(number);
   return display.textContent = number;
-}
-
-// const displayValueTwo = function(){
-//   let number = currentValue.toString().split(',').join('');
-//   displayedNumberMemoryTwo = Number(number);
-//   return display.textContent = number;
-// }
+};
 
 
 
@@ -95,90 +84,85 @@ btn_0.addEventListener('click', () => {
   currentValue.push(0);
   displayValueOne();
   
-})
+});
 
 btn_1.addEventListener('click', () => {
   currentValue.push(1);
   displayValueOne();
-})
+});
 
 btn_2.addEventListener('click', () => {
   currentValue.push(2);
   displayValueOne();
-})
+});
 
 btn_3.addEventListener('click', () => {
   currentValue.push(3);
   displayValueOne();
-})
+});
 
 btn_4.addEventListener('click', () => {
   currentValue.push(4);
   displayValueOne();
-  // display.textContent = 4;
-  // if(currentValue >= 0) {
-  //   currentValue = 4;
-  // }
-})
+});
 
 btn_5.addEventListener('click', () => {
   currentValue.push(5);
   displayValueOne();
-  // display.textContent = 5;
-  // if(currentValue >= 0) {
-  //   currentValue = 5;
-  // }
-})
+});
 
 btn_6.addEventListener('click', () => {
   currentValue.push(6);
   displayValueOne();
-  // display.textContent = 6;
-  // if(currentValue >= 0) {
-  //   currentValue = 6;
-  // }
-})
+});
 
 btn_7.addEventListener('click', () => {
   currentValue.push(7);
   displayValueOne();
-  // display.textContent = 7;
-  // if(currentValue >= 0) {
-  //   currentValue = 7;
-  // }
-})
+});
 
 btn_8.addEventListener('click', () => {
   currentValue.push(8);
   displayValueOne();
-  // display.textContent = 8;
-  // if(currentValue >= 0) {
-  //   currentValue = 8;
-  // }
-  // console.log(currentValue);
-})
+});
 
 btn_9.addEventListener('click', () => {
   currentValue.push(9);
   displayValueOne();
-  // display.textContent = 9;
-  // if(currentValue >= 0) {
-  //   currentValue = 9;
-  // }
-  // console.log(currentValue);
-})
+});
+
+
 
 btnAdd.addEventListener('click', () => {
   currentValue = [];
   displayedNumberMemoryTwo = displayedNumberMemoryOne;
+  return selectedOperator = operator.add;
+});
 
-  // let a = displayedNumberMemory;
-  // let number = numOne.toString().split(',').join('');
+btnSubtract.addEventListener('click', () => {
+  currentValue = [];
+  displayedNumberMemoryTwo = displayedNumberMemoryOne;
+  return selectedOperator = operator.subtract;
+});
 
-})
+btnMultiply.addEventListener('click', () => {
+  currentValue = [];
+  displayedNumberMemoryTwo = displayedNumberMemoryOne;
+  return selectedOperator = operator.multiply;
+});
+
+btnDivide.addEventListener('click', () => {
+  currentValue = [];
+  displayedNumberMemoryTwo = displayedNumberMemoryOne;
+  return selectedOperator = operator.divide;
+});
+
+btnEquals.addEventListener('click', () => {
+  operate(displayedNumberMemoryTwo, selectedOperator, displayedNumberMemoryOne);
+});
 
 btnClear.addEventListener('click', () => {
   display.textContent = '00000';
   currentValue = [];
-})
+});
 
